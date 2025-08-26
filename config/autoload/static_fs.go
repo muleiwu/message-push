1717e6d4c97316aa69f333f40c7be11e1cc351ff
mliev <service@mliev.com>
2025-08-26 12:04:21 +0800

    refactor(config/autoload/static_fs.go): 重命名配置键 "file.static" 为 "static.fs" 以统一命名规范

--- config/autoload/static_fs.go
+++ config/autoload/static_fs.go
@@ -11,7 +11,7 @@ type StaticFs struct {
 
 func (receiver StaticFs) InitConfig(helper envInterface.GetHelperInterface) map[string]any {
 	return map[string]any{
-		"file.static": func() map[string]embed.FS {
+		"static.fs": func() map[string]embed.FS {
 			return map[string]embed.FS{
 				// 这里会在启动的时候在 main.go 注入静态资源进来，请在main.go 添加静态资源
 				//"tempe": embed.FS{},
