128c17ea2cd1ebafeab3b613def9b9ea90ab3325
mliev <service@mliev.com>
2025-11-24 22:58:57 +0800

    fix(config): 将http静态模式从disk改为embed

--- config/autoload/http.go
+++ config/autoload/http.go
@@ -8,7 +8,7 @@ type Http struct {
 func (receiver Http) InitConfig(helper envInterface.HelperInterface) map[string]any {
 	return map[string]any{
 		"http.load_static": true,
-		"http.static_mode": "disk", // disk embed
+		"http.static_mode": "embed", // disk embed
 		"http.static_dir":  []string{"install", "admin"},
 	}
 }
