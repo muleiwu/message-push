ebac6df57b2a664121057466289a788356dbb8a0
mliev <service@mliev.com>
2025-11-27 21:58:18 +0800

    fix: 修正缓存驱动默认值配置问题，启用 redis 作为默认驱动 (develop)

--- config/autoload/cache.go
+++ config/autoload/cache.go
@@ -7,6 +7,6 @@ type Cache struct {
 
 func (receiver Cache) InitConfig(helper envInterface.HelperInterface) map[string]any {
 	return map[string]any{
-		"cache.driver": helper.GetEnv().GetString("database.driver", "none"), // memory,redis,none
+		"cache.driver": helper.GetEnv().GetString("cache.driver", "redis"), // memory,redis,none
 	}
 }
